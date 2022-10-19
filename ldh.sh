d021d754cf429a4d45a684e1e64ceeae94523fad
Emanuel Dramos <emanuel.dramos2@gmail.com>
2022-10-18 23:29:42 -0300

    Update

--- ldh.sh
+++ ldh.sh
@@ -6,53 +6,33 @@
 # Autor:      Emanuel Dramos
 # Manutenção: Dramos
 #
-# ------------------------------------------------------------------------ #
-#  Inicialmente criado para gerar um script com uma lista de leases DHCP
-# a ser aplicada no RouterOS Mikrotik.
-#
 #  Exemplos:
-#      $ ./nomeDoScript.sh -d 1
-#      Neste exemplo o script será executado no modo debug nível 1.
-# ------------------------------------------------------------------------ #
+#      $ ./ldh.sh -c arquivo.txt -o novo-arquivo.txt
+#      Neste exemplo o script irá gerar o arquivo novo-arquivo.txt com as alterações.
+#
 # Histórico:
 #
-#   v1.0 14/07/2021, Emanuel Dramos:
-#       -
-# ------------------------------------------------------------------------ #
+#   v1.0 14/07/2021, Emanuel Dramos
+#   v2.0 18/10/2022, Emanuel Dramos
+#
 # Testado em:
 #   bash 4.4.19
-# ------------------------------------------------------------------------ #
-# Agradecimentos:
-#
-# Joãozinho - Encontrou um bug na parte de login.
-# Mariazinha - Enviou uma sugestão de adicionar o -h.
-# ------------------------------------------------------------------------ #
+#   bash 5.1.4
 # ------------------------------- VARIÁVEIS ----------------------------------------- #
-# LDH_FILEOUT=0
-# LDH_SORT=0
-# LDH_TR=0
-#LDH_CH=0
+IP="/ip dhcp-server lease add address="
+COMMENT="comment="
+MAC="mac-address="
+SERVER="server="
 
-ALERT_ARQ=$(echo "
-    ALERTA! O script não pôde ser executado. Possíveis causas:
-
-        1 - Opção selecionada é inválida
-        2 - Argumento inválido
-        3 - Arquivo de destino existente.
-
-    Para ajuda utlize o comando com a opção -h
-    " && exit 1)
-
-LDH_FILEIN=0
-
-VERSION="v1.0"
-
-LDH_CMD="/ip dhcp-server lease add address=ipaddress comment=description mac-address=macaddress server=dhserver"
-
+ARQ_FILE=""
+CREATE=0
+MENUSC=0
+MENUSS=0
+MENUSSS=0
+MENUST=0
+MENUSTT=0
+VERSION="v2.0"
 LDH_HELP="
-
-##################################################################
-
   $(basename $0) - [OPÇÕES]
 
     -c Muda o leases dhcp do arquivo de origem e imprime na tela
@@ -66,165 +46,69 @@ LDH_HELP="
 
     Ex1: ldh.sh -c arquivo_origem.txt
     Ex2: ldh.sh -c arquivo_origem.txt -o -S arquivo-destino.txt
-
-##################################################################
 "
-# ------------------------------------------------------------------------ #
-
+ALERT_ARQ=$(echo "
+    ALERTA! O script não pôde ser executado. 
+    Para ajuda utlize a opção -h
+    " && exit 1)
 # ------------------------------- TESTES ----------------------------------------- #
-
-# ------------------------------------------------------------------------ #
-
+[ ! $1 ] && echo "$LDH_HELP"
+[ $1 = "-v" ] || [ $1 = "-h" ] && [ -n $2 ] && echo "$LDH_HELP" && exit 1
 # ------------------------------- FUNÇÕES ----------------------------------------- #
+Changer1 () {
 
-# ------------------------------------------------------------------------ #
+file1="$1"
 
+while IFS= read -r linha; do
+  echo $(echo -n $IP && echo -n $linha | cut -d " " -f2) \
+       $(echo -n $COMMENT && echo -n $linha | cut -d " " -f3) \
+       $(echo -n $MAC && echo -n $linha | cut -d " " -f1) \
+       $(echo -n $SERVER && echo -n $linha | cut -d " " -f4)
+done < "$file1"
+}
+
+Changer2 () {
+
+file1="$1"
+
+while IFS= read -r linha; do
+  echo $(echo -n $IP && echo -n $linha | cut -d " " -f2) \
+       $(echo -n $COMMENT && echo -n $(echo -n $linha | cut -d " " -f3 | tr [a-z] [A-Z])) \
+       $(echo -n $MAC && echo $(echo -n $linha | cut -d " " -f1 | tr [a-z] [A-Z])) \
+       $(echo -n $SERVER && echo -n $linha | cut -d " " -f4)
+done < "$file1"
+}
+
+Changer3 () {
+
+file1="$1"
+
+while IFS= read -r linha; do
+  echo $(echo -n $IP && echo -n $linha | cut -d " " -f2) \
+       $(echo -n $COMMENT && echo -n $(echo -n $linha | cut -d " " -f3 | tr [A-Z] [a-z])) \
+       $(echo -n $MAC && echo $(echo -n $linha | cut -d " " -f1 | tr [A-Z] [a-z])) \
+       $(echo -n $SERVER && echo -n $linha | cut -d " " -f4)
+done < "$file1"
+}
 # ------------------------------- EXECUÇÃO ----------------------------------------- #
+while [ -n "$1" ]; do
+    case $1 in
+        -c) MENUSC=1 && [ -f $2 ] && ARQ_FILE=$(echo "$2") ;;
+        -h) echo "$LDH_HELP" && exit 0 ;;
+        -o) CREATE=1 && [ ! -f $2 ] && DST=$(echo "$2") ;;
+        -s) MENUSS=1 && [ -f $2 ] && ARQ_FILE=$(echo "$2") ;;
+        -S) MENUSSS=1 && [ -f $2 ] && ARQ_FILE=$(echo "$2") ;;
+        -t) MENUST=1 && [ -f $2 ] && ARQ_FILE=$(echo "$2") ;;
+        -T) MENUSTT=1 && [ -f $2 ] && ARQ_FILE=$(echo "$2") ;;
+        -v) echo "$VERSION" && exit 0 ;;
+    esac
+    shift
+done
 
-
-case "$1" in
-#########ldh -c arquivo-origem
-  -c) if [ -f "$2" ] && [ -z "$3" ]; then
-        LDH_FILEIN=$(sed -n '1,$p' "$2" | awk -F " " '{print $1 "@" $2 "@" $3 "@" $4}')
-        echo $LDH_FILEIN
-        for d in $LDH_FILEIN
-          do
-            echo "$LDH_CMD" | \
-            sed "s/ipaddress/$d/g; s/description/$d/g; s/macaddress/$d/g; s/dhserver/$d/g" | \
-            sed "s/=/@/g" | sed "s/@/=/g" | \
-            awk -F "=" '{print $1 "=" $3 " " $5 "=" $8 " " $9 "=" $10}' | \
-            awk -F " " '{print $1 " " $2 " " $3 " " $4 " "$5 " " $7 " " $9}'
-          done
-          exit 0
-
-      fi
-
-       case "$3" in
-# #########ldh -c arquivo-origem -o arquivo-destino (se arquivo-destino existir cancela)
-         -o)
-# #########ldh -c arquivo-origem -o arquivo-destino
-             if [ -n "$4" ] && [ ! -f "$4" ] && [ -z $5 ]; then
-#
-               LDH_FILEIN=$(sed -n '1,$p' "$2" | awk -F " " '{print $1 "@" $2 "@" $3 "@" $4}')
-#
-               for d in $LDH_FILEIN
-                 do
-                   echo "$LDH_CMD" | \
-                   sed "s/ipaddress/$d/g; s/description/$d/g; s/macaddress/$d/g; s/dhserver/$d/g" | \
-                   sed "s/=/@/g" | sed "s/@/=/g" | \
-                   awk -F "=" '{print $1 "=" $3 " " $5 "=" $8 " " $9 "=" $10}' | \
-                   awk -F " " '{print $1 " " $2 " " $3 " " $4 " "$5 " " $7 " " $9}' \
-                   >> $4
-                 done
-                   exit 0
-
-             fi
-
-             case "$4" in
- #########ldh -c arquivo-origem -o -s arquivo-destino (se arquivo-destino existir cancela)
-                -s) if [ -f "$5" ]; then
-                      echo "$ALERT_ARQ" && exit 0
-                    fi
- #########ldh -c arquivo-origem -o -s arquivo-destino
-                   if [ -n "$5" ] && [ ! -f "$5" ]; then
-
-                     LDH_FILEIN=$(sed -n '1,$p' "$2" | awk -F " " '{print $1 "@" $2 "@" $3 "@" $4}')
-
-                     for d in $LDH_FILEIN
-                       do
-                         echo "$LDH_CMD" | \
-                         sed "s/ipaddress/$d/g; s/description/$d/g; s/macaddress/$d/g; s/dhserver/$d/g" | \
-                         sed "s/=/@/g" | sed "s/@/=/g" | \
-                         awk -F "=" '{print $1 "=" $3 " " $5 "=" $8 " " $9 "=" $10}' | \
-                         awk -F " " '{print $1 " " $2 " " $3 " " $4 " "$5 " " $7 " " $9}' \
-                         >> $5.tmp
-                         sort -t "=" -k3 "$5.tmp" > $5
-                       done
-                       rm "$5.tmp"
-                       exit 0
-
-                   fi ;;
-
-  #########ldh -c arquivo-origem -o -S arquivo-destino (se arquivo-destino existir cancela)
-                -S) if [ -f "$5" ]; then
-                      echo "$ALERT_ARQ" && exit 0
-                    fi
- #########ldh -c arquivo-origem -o -S arquivo-destino
-                     if [ -n "$5" ] && [ ! -f "$5" ]; then
-                     LDH_FILEIN=$(sed -n '1,$p' "$2" | awk -F " " '{print $1 "@" $2 "@" $3 "@" $4}')
-
-                     for d in $LDH_FILEIN
-                       do
-                         echo "$LDH_CMD" | \
-                         sed "s/ipaddress/$d/g; s/description/$d/g; s/macaddress/$d/g; s/dhserver/$d/g" | \
-                         sed "s/=/@/g" | sed "s/@/=/g" | \
-                         awk -F "=" '{print $1 "=" $3 " " $5 "=" $8 " " $9 "=" $10}' | \
-                         awk -F " " '{print $1 " " $2 " " $3 " " $4 " "$5 " " $7 " " $9}' \
-                         >> $5.tmp
-                         sort -n -t "=" -k2 "$5.tmp" > $5
-                       done
-                       rm "$5.tmp"
-                       exit 0
-
-               fi ;;
-
- #########ldh -c arquivo-origem -o -t arquivo-destino (se arquivo-destino existir cancela)
-                -t) if [ -f "$5" ]; then
-                      echo "$ALERT_ARQ" && exit 0
-                    fi
- #########ldh -c arquivo-origem -o -t arquivo-destino
-                   if [ -n "$5" ] && [ ! -f "$5" ]; then
-                     LDH_FILEIN=$(sed -n '1,$p' "$2" | awk -F " " '{print $1 "@" $2 "@" $3 "@" $4}')
-
-                     for d in $a $(echo "$LDH_FILEIN"  | tr [A-Z] [a-z])
-                       do
-                         echo "$LDH_CMD" | \
-                         sed "s/ipaddress/$d/g; s/description/$d/g; s/macaddress/$d/g; s/dhserver/$d/g" | \
-                         sed "s/=/@/g" | sed "s/@/=/g" | \
-                         awk -F "=" '{print $1 "=" $3 " " $5 "=" $8 " " $9 "=" $10}' | \
-                         awk -F " " '{print $1 " " $2 " " $3 " " $4 " "$5 " " $7 " " $9}' \
-                         >> $5
-                       done
-                         exit 0
-                   fi ;;
-
- #########ldh -c arquivo-origem -o -T arquivo-destino (se arquivo-destino existir cancela)
-                -T) if [ -f "$5" ]; then
-                      echo "$ALERT_ARQ" && exit 0
-                    fi
- #########ldh -c arquivo-origem -o -T arquivo-destino
-                   if [ -n "$5" ] && [ ! -f "$5" ]; then
-                     LDH_FILEIN=$(sed -n '1,$p' "$2" | awk -F " " '{print $1 "@" $2 "@" $3 "@" $4}')
-
-                     for d in $(echo "$LDH_FILEIN"  | tr [a-z] [A-Z])
-                       do
-                         echo "$LDH_CMD" | \
-                         sed "s/ipaddress/$d/g; s/description/$d/g; s/macaddress/$d/g; s/dhserver/$d/g" | \
-                         sed "s/=/@/g" | sed "s/@/=/g" | \
-                         awk -F "=" '{print $1 "=" $3 " " $5 "=" $8 " " $9 "=" $10}' | \
-                         awk -F " " '{print $1 " " $2 " " $3 " " $4 " "$5 " " $7 " " $9}' \
-                         >> $5
-                       done
-                       exit 0
-                   fi ;;
-
-             esac
-
-             if [ -n "$4" ] && [ -f "$4" ]; then
-                   echo "$ALERT_ARQ" && exit 0
-             fi ;;
-
-         *) echo "$ALERT_ARQ" && exit 0
-
-       esac ;;
-
- #########ldh -h
-   -h) echo "$LDH_HELP" && exit 0 ;;
-
- #########ldh -v
-   -v) echo "$VERSION" && exit 0 ;;
-
-esac
-
- #########Nenhuma opção válida
-echo "$ALERT_ARQ"
+[ $MENUSC -eq 1 ] && CHANGER="$(Changer1 $(echo "$ARQ_FILE"))"
+[ $MENUST -eq 1 ] && CHANGER="$(Changer3 $(echo "$ARQ_FILE"))"
+[ $MENUSTT -eq 1 ] && CHANGER="$(Changer2 $(echo "$ARQ_FILE"))"
+[ $MENUSS -eq 1 ] && CHANGER="$(echo "$CHANGER" | sort -t "=" -k3)"
+[ $MENUSSS -eq 1 ] && CHANGER="$(echo "$CHANGER" | sort -n -t "=" -k2)"
+[ $CREATE -eq 1 ] && echo "$CHANGER" > $DST
+echo "$CHANGER"
